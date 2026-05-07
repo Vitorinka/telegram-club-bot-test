0c59e66a113680bda8ba2bf1d9cc8a53d4c00112
Vitorinka <mishurvika@gmail.com>
2026-05-07 11:42:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ stripe.api_key = os.getenv("STRIPE_API_KEY")
 if not DATABASE_URL:
     raise ValueError("Критическая ошибка: DATABASE_URL не задан!")
 
-PHOTO_URL_INTRO = "https://imgur.com/a/TnzeGgh"
-PHOTO_URL_RULES = "https://imgur.com/a/TnzeGgh#hYLze9s"
+PHOTO_URL_INTRO = "https://i.imgur.com/a/TnzeGgh.jpg"
+PHOTO_URL_RULES = "https://i.imgur.com/a/TnzeGgh#hYLze9s.jpg"
 bot = Bot(token=BOT_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
